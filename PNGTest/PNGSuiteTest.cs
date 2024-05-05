780cfaefd13e3da6c18477c9a323ae42973e45f5
Sjon Kosse <sjon@outlook.com>
2024-05-06 00:00:41 +0200

    Repaired outputting

--- PNGTest/PNGSuiteTest.cs
+++ PNGTest/PNGSuiteTest.cs
@@ -10,14 +10,18 @@ public class PNGSuiteTest
     {
         var basicFiles = Directory.EnumerateFiles("samples").Where(x => Path.GetFileName(x).StartsWith(s));
         Directory.CreateDirectory("output");
-        foreach (var file in basicFiles)
+        foreach (var fileName in basicFiles)
         {
-            var fileData = File.ReadAllBytes(file);
+            var fileData = File.ReadAllBytes(fileName);
             var png = new PNG();
 
-            var stuff = png.ReadImage(fileData);
-            using var fs = new FileStream(Path.Combine("output", file), FileMode.Create, FileAccess.Write);
+            using var stuff = png.ReadImage(fileData);
+            stuff.Flush();
+            var path = Path.Combine("output", Path.ChangeExtension(fileName, ".ppm"));
+            using var fs = new FileStream(path, FileMode.Create);
+            stuff.Position = 0;
             stuff.CopyTo(fs);
+            fs.Flush();
         }
     }
 
